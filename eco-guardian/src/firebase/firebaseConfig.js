f6786e7b9742775cf8fab5174be69ee500d5a0c1
Kingsley Boateng <48519565+kingboatgh@users.noreply.github.com>
2026-03-13 03:56:58 +0000

    Updated firebaseConfig.js

--- eco-guardian/src/firebase/firebaseConfig.js
+++ eco-guardian/src/firebase/firebaseConfig.js
@@ -1,17 +1,14 @@
-// src/firebaseConfig.js
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// 🔥 PASTE your REAL config from Firebase console here
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "Eco-guardian 71256",
-  projectId: "eco-guardian-71256",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
